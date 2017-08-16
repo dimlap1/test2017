c27f2d2e4c6ce690afbe3f20cba0a989efa104d1
dimlap1 <dimlap@mail.ru>
2017-08-16 14:34:27 +0500

    160817

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -10,13 +10,26 @@ package javaapplication1;
  * @author lape
  */
 public class JavaApplication1 {
-
-    /**
+    
+        /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       System. out. println ("Hello World");
+        
+        int x=3;
+        
+        if (x==2)  {
+          System. out. println ("Hello World");  
+        }
+ while (x<10) {
+  System. out. println (x+" Hello");
+  x=x+1;
+ }
+         
+         
+         
+       
        
        System. out. println ("2 World");
        System. out. println ("3 World");
